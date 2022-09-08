929000b474802a8e1b38c16ae2c60c3f0947874e
syahrilr <syahrilramadhan754@gmail.com>
2022-09-08 13:48:26 +0800

    route kategori dan view awal

---
+++ resources/views/perjanjian.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+
+@section('content')
+    
+    <table>
+        <tr>
+            <th>Uraian</th>
+            <th>NO. PKS</th>
+            <th>Mulai</th>
+            <th>Berakhir</th>
+            <th>Wilayah</th>
+            <th>Kegiatan</th>
+            <th>Keterangan</th>
+        </tr>
+        @foreach ($perjanjians as $perjanjian)
+        <tr>
+            <td>{{ $perjanjian->uraian }}</td>
+            <td>{{ $perjanjian->no_pks }}</td>
+            <td>{{ $perjanjian->mulai }}</td>
+            <td>{{ $perjanjian->berakhir }}</td>
+            <td>{{ $perjanjian->wilayah }}</td>
+            <td>{{ $perjanjian->kegiatan }}</td>
+            <td>{{ $perjanjian->keterangan }}</td>
+        </tr> 
+        @endforeach
+        
+    </table>
+@endsection
