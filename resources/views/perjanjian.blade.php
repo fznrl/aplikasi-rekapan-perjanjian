9f89dfdab65119fa7f910b79a28b4bac64875cb9
syahrilr <syahrilramadhan754@gmail.com>
2022-11-11 18:42:18 +0800

    Memperbaiki Tampilan Dashboard

--- resources/views/perjanjian.blade.php
+++ resources/views/perjanjian.blade.php
@@ -6,33 +6,6 @@
 @endpush
 
 @section('content')
-<<<<<<< HEAD
-    {{ $title }}
-    <table>
-        <tr>
-            <th>Uraian</th>
-            <th>NO. PKS</th>
-            <th>Mulai</th>
-            <th>Berakhir</th>
-            <th>Wilayah</th>
-            <th>Kegiatan</th>
-            <th>Keterangan</th>
-        </tr>
-        @foreach ($perjanjians as $perjanjian)
-        <tr>
-            <td>{{ $perjanjian->uraian }}</td>
-            <td>{{ $perjanjian->no_pks }}</td>
-            <td>{{ $perjanjian->mulai }}</td>
-            <td>{{ $perjanjian->berakhir }}</td>
-            <td>{{ $perjanjian->wilayah }}</td>
-            <td>{{ $perjanjian->kegiatan }}</td>
-            <td>{{ $perjanjian->keterangan }}</td>
-        </tr> 
-        @endforeach
-        
-    </table>
-@endsection
-=======
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -142,4 +115,3 @@
 //  
 </script>
 @endpush
->>>>>>> fff10b8b0ead902b855c4a432b39d968cfc5d109
